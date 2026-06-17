4acb0a11e24d50c9d5ca12e754388a8f32c13f18
LilKoku <macirk24@gmail.com>
2026-06-17 23:57:52 +0200

    postacie i api

---
+++ lib/services/api_service.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../../rick_and_morty_explorer/lib/models/character.dart';
+
+class ApiService {
+  static const String baseUrl =
+      "https://rickandmortyapi.com/api";
+
+  Future<List<Character>> getCharacters() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/character"),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception("Błąd pobierania postaci");
+    }
+
+    final data = jsonDecode(response.body);
+
+    return (data["results"] as List)
+        .map((e) => Character.fromJson(e))
+        .toList();
+  }
+
+  Future<Character> getCharacterDetails(int id) async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/character/$id"),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception("Błąd pobierania szczegółów");
+    }
+
+    return Character.fromJson(
+      jsonDecode(response.body),
+    );
+  }
+}
